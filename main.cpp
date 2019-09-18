68ae5664d77a641b0eace7a759442fefd79de30d
lenington <lenitorios@gmail.com>
2019-09-18 00:28:21 -0300

    Ultimos ajustes

--- main.cpp
+++ main.cpp
@@ -16,8 +16,7 @@ using namespace std;
 */
 
 int main() {
-    // Variables
-    Mat img, threshImg;
+    Mat imagem, frame_threshold;
     string entrada, saida, arquivo;
 
     int somaTotal = 0; //soma total de todos os pontos encontrados
@@ -26,43 +25,47 @@ int main() {
     arquivo = "dices2.png"; //nome da imagem
     entrada = "images/"+arquivo;
     saida = "images/output/"+arquivo;
-    img = imread(entrada);
+    imagem = imread(entrada);
 
     namedWindow("Imagem Original", WINDOW_NORMAL); // define a janela
     //namedWindow('image',WINDOW_NORMAL);
-    imshow("Imagem Original", img);
+    imshow("Imagem Original", imagem);
     resizeWindow("Imagem Original", 600,600);
 
     // Para o threshold
-    inRange(img, Scalar(200, 200, 200), Scalar(255, 255, 255), threshImg);
+    //adaptado de: https://docs.opencv.org/3.4/da/d97/tutorial_threshold_inRange.html
+    inRange(imagem, Scalar(200, 200, 200), Scalar(255, 255, 255), frame_threshold);
 
     namedWindow("Threshold da Imagem", WINDOW_NORMAL);
-    imshow("Threshold da Imagem", threshImg);
+    imshow("Threshold da Imagem", frame_threshold);
     resizeWindow("Threshold da Imagem", 600,600);
 
     // Salva na pasta de threshold
-    imwrite("images/threshold/"+arquivo, threshImg);
+    imwrite("images/threshold/"+arquivo, frame_threshold);
 
-    // Removendo pequenas regioes
-    Mat element = getStructuringElement(MORPH_RECT, Size(6, 6), Point(1, 1));
-    erode(threshImg, threshImg, element);
-    dilate(threshImg, threshImg, element);
+    // Removendo pequenas regioes (retangulos)
+    //adaptado de: https://docs.opencv.org/3.4/d4/d86/group__imgproc__filter.html
+    Mat rect = getStructuringElement(MORPH_RECT, Size(6, 6), Point(1, 1));
+    //Eliminando as particulas indesejadas o retangulo
+    //(Abertura = edorir e depois dilatar o resultado da erosao)
+    erode(frame_threshold, frame_threshold, rect);
+    dilate(frame_threshold, frame_threshold, rect);
 
     // Pegando os contornos
     //Adaptado de: https://docs.opencv.org/2.4/doc/tutorials/imgproc/shapedescriptors/find_contours/find_contours.html
     vector<vector<Point> > contornos; //contorno dos dados
-    vector<Vec4i> hierarchy;
-    findContours(threshImg, contornos, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    vector<Vec4i> hierarchy; //
+    findContours(frame_threshold, contornos, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-    int countPontos = 0;
-    int ultimoPonto = 0;
+    int countPontos = 0; //contador de pontos para cada dado
+    int ultimoPonto = 0; //
     double tamanhoDado = 0;
     int debugDado = 0; //variavel apenas para debug de ordem
 
     for(int i = 0; i < contornos.size(); i++){
         double areaContornoDado = contourArea(contornos[i]); //pega a area de contorno
 
-        if(areaContornoDado > 10000 && areaContornoDado < 20000){
+        if(areaContornoDado > 10000 && areaContornoDado < 20000){ //limita a area do contorno do dado
             tamanhoDado = areaContornoDado;
             //reinicia a contagem
             countPontos = 0;
@@ -73,13 +76,13 @@ int main() {
         }
 
         //se o contorno estiver dentro da area do dado, ele possuira uma porcentagem
-        //tirando por regra de tres, essa porcentagem geralmente esta entre:
+        //tirado por regra de tres, essa porcentagem geralmente esta entre:
         //0,029 e 0,045
         double x = areaContornoDado/tamanhoDado;
 
         if((x >= 0.019 && x <= 0.075) && (tamanhoDado > 10000 && tamanhoDado < 20000)){
 
-            if (hierarchy[i][3] == -1){
+            if (hierarchy[i][3] == -1){ //significa que o contorno do dado acabou
                 // Reinicia quando um novo contorno eh encontrado
                 countPontos = 0;
                 ultimoPonto = i;
@@ -87,7 +90,7 @@ int main() {
 
                 countPontos += 1;
 
-                // Verifica dentro do contorno se eh o ultimo ponto
+                // Verifica dentro do contorno se eh o ultimo ponto (hierarchy[i + 1][3] == -1)
                 if ((i == contornos.size() - 1) || (hierarchy[i + 1][3] == -1)){
                     somaTotal += countPontos;
                     cout << "Dado de ";
@@ -98,11 +101,11 @@ int main() {
 
                     stringstream stream;
                     stream << countPontos;
-                    Moments m = moments(contornos[ultimoPonto]);
+                    Moments m = moments(contornos[ultimoPonto]); //centro do contorno
 
                     //adaptado de: https://docs.opencv.org/3.4.7/dc/dcf/tutorial_js_contour_features.html
                     Point p = Point(m.m10 / m.m00 + 10, m.m01 / m.m00);
-                    putText(img, stream.str(), p, FONT_HERSHEY_PLAIN, 2, Scalar(255,0,0),3);
+                    putText(imagem, stream.str(), p, FONT_HERSHEY_PLAIN, 2, Scalar(255,0,0),3);
 
                     countPontos = 0;
                     ultimoPonto = i;
@@ -117,7 +120,7 @@ int main() {
                     cout << "\n\n";*/
                 }
             }
-            drawContours(img, contornos, i, Scalar(0,128,0), 2, 8, hierarchy, 0, Point());
+            drawContours(imagem, contornos, i, Scalar(0,128,0), 2, 8, hierarchy, 0, Point());
 
             //cout << countPontos;
             //cout << "\n";
@@ -128,13 +131,13 @@ int main() {
     // Escrevendo o resultado na imagem
     stringstream stream;
     stream << "Total: " << somaTotal;
-    putText(img, stream.str(), Point(30, 50), FONT_HERSHEY_PLAIN, 2, Scalar(0,0,0), 3);
+    putText(imagem, stream.str(), Point(30, 50), FONT_HERSHEY_PLAIN, 2, Scalar(0,0,0), 3);
 
     // Salva na pasta de output o resultado final
-    imwrite(saida, img);
+    imwrite(saida, imagem);
 
     namedWindow("Resultado da Imagem", WINDOW_NORMAL);
-    imshow("Resultado da Imagem", img);
+    imshow("Resultado da Imagem", imagem);
     resizeWindow("Resultado da Imagem", 600,600);
 
 
